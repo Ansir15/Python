2807f005d96a5dad8ff9859894dfc81b2c4270d0
Ansir Ali <ansir1869@gmail.com>
2025-08-29 16:39:07 +0500

    Add files via upload

--- Advance_pyhon_02.py
+++ Advance_pyhon_02.py
@@ -1,90 +1,88 @@
-# # lambda function
-# # without lambda
-# def square(n):
-#     return n*n
-# print(square(5))
-# # with lambda function
-# square=lambda x:x*x
-# print(square(5))
+# lambda function
+# without lambda
+def square(n):
+    return n*n
+print(square(5))
+# with lambda function
+square=lambda x:x*x
+print(square(5))
 
-# # join method only for string
-# list=["Ansir","umair","daud"]
-# final="::".join(list)
-# print(final)
+# join method only for string
+list=["Ansir","umair","daud"]
+final="::".join(list)
+print(final)
 
-# # format method
-# a="{0} is a good {1}".format("Ansir" , "boy")
-# print(a)
+# format method
+a="{0} is a good {1}".format("Ansir" , "boy")
+print(a)
 
 
-# # map 
+# map 
 
-# l=[1,2,3,4,5]
-# square=list(map(lambda x:x*x , l))
-# print(square)
+l=[1,2,3,4,5]
+square=list(map(lambda x:x*x , l))
+print(square)
 
-# # fiter
+# fiter
 
-# l=[1,2,3,4,5,6,7,8]
-# even=list(filter(lambda x:x%2==0 , l))
-# print(even)
+l=[1,2,3,4,5,6,7,8]
+even=list(filter(lambda x:x%2==0 , l))
+print(even)
 
-# # reduce
+# reduce
 from  functools import reduce
-# l=[1,2,3,4,5]
-# sum_numbers=reduce(lambda acc,x:acc+x ,l, 0)
-# print(sum_numbers)
+l=[1,2,3,4,5]
+sum_numbers=reduce(lambda acc,x:acc+x ,l, 0)
+print(sum_numbers)
 
-# # pracice questions
-# # Q 02
+# pracice questions
+# Q 02
 
-# a=" the name of the students is {0} , his marks are {1} and phone number is {2}".format("Ansir",95,"454574050")
-# print(a)
+a=" the name of the students is {0} , his marks are {1} and phone number is {2}".format("Ansir",95,"454574050")
+print(a)
 
-# # Q 03
+# Q 03
 
-# table= [str(7*i) for i in range(1,11)]
-# s="\n".join(table)
-# print(s)
+table= [str(7*i) for i in range(1,11)]
+s="\n".join(table)
+print(s)
 
-# # Q 04
-# l=[111,25,34,44,45,566,67,78]
-# even=list(filter(lambda x:x%5==0 , l))
-# print(even)
-
-
-# # Q 05 
-# l=[111,25,34,44,45,566,67,78]
-# maximum=reduce(lambda a,b:a if a>b else b ,l, 0)
-# print(f"Maximum number : {maximum}")
-
-# # OR
-# l=[111,25,34,44,45,566,67,78] 
-# def maximum_num(a,b):
-#     if (a>b):
-#         return a
-#     return b
-# print(reduce(maximum_num,l,0))
+# Q 04
+l=[111,25,34,44,45,566,67,78]
+even=list(filter(lambda x:x%5==0 , l))
+print(even)
 
 
-# # Q 06 install freeze
+# Q 05 
+l=[111,25,34,44,45,566,67,78]
+maximum=reduce(lambda a,b:a if a>b else b ,l, 0)
+print(f"Maximum number : {maximum}")
 
-# # Q 07 
-# from flask import Flask
+# OR
+l=[111,25,34,44,45,566,67,78] 
+def maximum_num(a,b):
+    if (a>b):
+        return a
+    return b
+print(reduce(maximum_num,l,0))
 
-# app = Flask(__name__)
 
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
+# Q 06 install freeze
+
+# Q 07 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 # app.run()
  
 
 
 from flask import Flask, request
 from markupsafe import escape
-
-# ✅ yeh line missing thi tumhari code me
 app = Flask(__name__)
 
 @app.route("/hello")
